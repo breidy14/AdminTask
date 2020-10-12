8a2d98eccd023e25c194074ad7dd3f7492d00e9e
Breidy Nuñez <breidy.manuel@gmail.com>
2020-10-12 16:51:17 -0400

    Add onDelete:cascade

--- migrations/20201008184003-create-task-category.js
+++ migrations/20201008184003-create-task-category.js
@@ -1,38 +1,42 @@
-'use strict';
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    await queryInterface.createTable('TaskCategories', {
-      id: {
-        allowNull: false,
-        autoIncrement: true,
-        primaryKey: true,
-        type: Sequelize.INTEGER
-      },
-      taskId: {
-        type: Sequelize.INTEGER,
-        references:{
-          model:{tableName: 'Tasks'},
-          key: 'id'
+    'use strict';
+    module.exports = {
+    up: async (queryInterface, Sequelize) => {
+        await queryInterface.createTable('TaskCategories', {
+        id: {
+            allowNull: false,
+            autoIncrement: true,
+            primaryKey: true,
+            type: Sequelize.INTEGER
+        },
+        taskId: {
+            type: Sequelize.INTEGER,
+            references:{
+            model:{tableName: 'Tasks'},
+            key: 'id',
+            onDelete: 'cascade',
+            onUpdate: 'cascade'
+            }
+        },
+        categoryId: {
+            type: Sequelize.INTEGER,
+            references:{
+            model:{tableName: 'Categories'},
+            key: 'id',
+            onDelete: 'cascade',
+            onUpdate: 'cascade'
+            }
+        },
+        createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE
+        },
+        updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE
         }
-      },
-      categoryId: {
-        type: Sequelize.INTEGER,
-        references:{
-          model:{tableName: 'Categories'},
-          key: 'id'
-        }
-      },
-      createdAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      },
-      updatedAt: {
-        allowNull: false,
-        type: Sequelize.DATE
-      }
-    });
-  },
-  down: async (queryInterface, Sequelize) => {
-    await queryInterface.dropTable('TaskCategories');
-  }
-};
+        });
+    },
+    down: async (queryInterface, Sequelize) => {
+        await queryInterface.dropTable('TaskCategories');
+    }
+    };
